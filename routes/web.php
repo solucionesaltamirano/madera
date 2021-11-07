9b6c17678622b3e778ef0034e6103b31b0e8a6b6
gersonaltamirano <gersonaltamirano@gmail.com>
2021-11-07 12:24:54 -0600

    login for external login - facebook

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,8 @@ Route::get('/login-facebook', function () {
 Route::get('/facebook-callback', function () {
     $user = Socialite::driver('facebook')->user();
 
+    dd($user);
+
     $userExist = User::where('external_id', $user->id)->where('external_auth', 'facebook')->first();
 
     if($userExist){
