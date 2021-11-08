d85df568b9a6cc2b9fab8d5fffcc2bc324a935e9
gersonaltamirano <gersonaltamirano@gmail.com>
2021-11-08 12:52:02 -0600

    favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        
+        <link rel="icon" type="image/png" href="img/favicon.png"/>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
