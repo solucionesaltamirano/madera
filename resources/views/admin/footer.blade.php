63db037488dbd07664f4747125ef452ca05c86ee
gersonaltamirano <gersonaltamirano@gmail.com>
2021-11-10 20:02:54 -0600

    use other Admin LTE more control

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    Copyright &copy; 2014-2021 <a href="https://solucionesaltamirano.com">Soluciones Altamirano</a>.
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.1.0
+    </div>
+</footer>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,4 +1,86 @@
-@extends('adminlte::page')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ $header }} | {{ config('app.name', 'Laravel') }}</title>
+
+        @include('admin.link-style')
+    
+        <!-- Alpine js -->
+        <script src="//unpkg.com/alpinejs" defer></script>
+
+        <!-- livewire -->
+        @livewireScripts
+
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+        <!-- Preloader -->
+        @include('admin.preloader')
+
+        <!-- Navbar -->
+        @include('admin.navbar')
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        @include('admin.sidebar')
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">
+                                Dashboard 
+                                <i class="fal fa-tachometer-slow"></i>
+                            </h1>
+                            
+                        </div><!-- /.col -->
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">Dashboard v1</li>
+                            </ol>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid border">
+                    @livewire('test')
+                </div><!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+        @include('admin.footer')
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    @include('admin.javascritp')
+</body>
+
+</html>
+
+
+
+
+
+{{-- @extends('adminlte::page')
 
 @if (isset($header))
     @section('title')
@@ -51,15 +133,10 @@
 
 @section('css')
     <link href="https://kit-pro.fontawesome.com/releases/v5.15.4/css/pro.min.css" rel="stylesheet">
-    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}"/>
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-@stop
+    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}"/> --}}
+{{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+{{-- @stop
 
 @section('js')
     <script src="//unpkg.com/alpinejs" defer></script>
-@stop
-
-
-
-
-
+@stop --}}
