abf2918cf1d1ae7348e65b3a7132af97644d9864
gersonaltamirano <gersonaltamirano@gmail.com>
2021-11-17 12:10:41 -0600

    test modal

--- resources/views/livewire/test.blade.php
+++ resources/views/livewire/test.blade.php
@@ -19,8 +19,29 @@
         <div class="btn btn-outline-info" x-on:click="$wire.emit('showToast', 'TITLE INFO', 'info')">info</div>
     </div>
 
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
+
     @livewire('partial.toast', [
         'title' => 'This is info Toast',
         'icon' => 'info',
     ])
+
+    <x-modal>
+
+        <x-slot name="title">
+            Modal title
+        </x-slot>
+
+        <x-slot name="body">
+            Modal body text goes here.
+        </x-slot>
+
+        <x-slot name="footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </x-slot>
+    </x-modal>
+
+
+
 </div>
