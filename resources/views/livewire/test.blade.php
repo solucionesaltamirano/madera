10b3f8f5b4b2faef6d18f9338a15e29b93ef6bc4
gersonaltamirano <gersonaltamirano@gmail.com>
2021-11-17 10:01:18 -0600

    testing toast

--- resources/views/livewire/test.blade.php
+++ resources/views/livewire/test.blade.php
@@ -12,5 +12,15 @@
         <div>
             Alpine: <span x-text="countAlpine"></span>
         </div>
+
+        <div class="btn btn-outline-success"x-on:click="$wire.emit('showToast', 'TITLE SUCCESS')">success</div>
+        <div class="btn btn-outline-danger" x-on:click="$wire.emit('showToast', 'TITLE ERROR', 'error')">error</div>
+        <div class="btn btn-outline-warning" x-on:click="$wire.emit('showToast', 'TITLE WARNING', 'warning')">warning</div>
+        <div class="btn btn-outline-info" x-on:click="$wire.emit('showToast', 'TITLE INFO', 'info')">info</div>
     </div>
+
+    @livewire('partial.toast', [
+        'title' => 'This is info Toast',
+        'icon' => 'info',
+    ])
 </div>
