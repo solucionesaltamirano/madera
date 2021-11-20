6e77668a25387bac8756a25d0b7fa5a295cbb77d
gersonaltamirano <gersonaltamirano@gmail.com>
2021-11-20 17:32:16 -0600

    confirm delete with sweet alert 3

---
+++ resources/views/partials/confirm-delete.blade.php
@@ -0,0 +1,25 @@
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script>
+
+    $('.deleteConfirm').submit(function(e){
+        e.preventDefault()
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Swal.fire(
+                //     'Deleted!',
+                //     'Your file has been deleted.',
+                //     'success'
+                // )
+                this.submit();
+            }
+        })
+    })
+</script>
