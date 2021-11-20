e0168adfed8e4c2d88fe92e840e4281db7da5ed8
gersonaltamirano <gersonaltamirano@gmail.com>
2021-11-20 00:06:38 -0600

    execute backup database at 08:00 hrs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('database:backup')
-        ->daily();
+        ->dailyAt('08:00');
         // ->everyMinute();
     }
 
