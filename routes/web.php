34e95d54b448f4f0c81d037e9aa3210ae8d041d5
Gerson Altamirano <gersonaltamirano@gmail.com>
2021-12-07 10:47:51 -0600

    fix register nickname

--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,6 @@ Route::get('/login-google', function () {
 Route::get('/google-callback', function () {
     $externalUser = Socialite::driver('google')->user();
 
-
-    dd($externalUser);
-
     $userExist = ExternalAuth::where('external_id', $externalUser->id)->where('external_auth', 'google')->first();
     
     if($userExist){
