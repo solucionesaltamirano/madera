2fc8eabebcb6a3815b8e42b3be395f596cb6e85b
Gerson Altamirano <gersonaltamirano@gmail.com>
2021-12-09 17:16:55 -0600

    better ui layout and stub

--- resources/views/admin/page-header.blade.php
+++ resources/views/admin/page-header.blade.php
@@ -1,19 +1,35 @@
 <!-- Content Header (Page header) -->
 <div class="content-header">
     <div class="container-fluid">
-        <div class="row mb-2">
+        <div class="row ">
             <div class="col-sm-6">
-                <h1 class="m-0 ">
-                    {{ $pageHeader }}
-                </h1>
+                @if(isset($pageHeader))
+                    <h1 class="m-0 ">
+                        {{ $pageHeader }}
+                    </h1>
+                @endif
                 
             </div><!-- /.col -->
-            <div class="col-sm-6">
+            @if(isset($breadcrumbs))
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        @foreach($breadcrumbs as $breadcrumb)
+                            @if($breadcrumb['active'])
+                                <li class="breadcrumb-item active">{{ $breadcrumb['name'] }}</li>
+                            @else
+                                <li class="breadcrumb-item"><a href="{{ $breadcrumb['url'] }}">{{ $breadcrumb['name'] }}</a></li>
+                            @endif
+                        @endforeach
+                    </ol>
+                </div><!-- /.col -->
+            @endif
+            {{-- <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
                     <li class="breadcrumb-item active">Dashboard v1</li>
                 </ol>
-            </div><!-- /.col -->
+            </div> --}}
+            <!-- /.col -->
         </div><!-- /.row -->
     </div><!-- /.container-fluid -->
 </div>
