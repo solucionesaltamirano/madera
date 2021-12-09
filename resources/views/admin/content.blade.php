ac6039a46ac977fd9d91d72c07d16d802c6ba3f9
Gerson Altamirano <gersonaltamirano@gmail.com>
2021-12-09 17:16:45 -0600

    better ui layout and stub

--- resources/views/admin/content.blade.php
+++ resources/views/admin/content.blade.php
@@ -2,16 +2,14 @@
     <div class="card card-primary card-outline min-vh-100">
         <div class="card-header">
             <div class="card-title w-100">
-                <div class="row ">
-                    <div class="col strong pt-1">
-                        {{ $cardTitle }}
+                @if(isset($cardTitle))
+                    {{ $cardTitle }}
+                @endif
+                @if(isset($cardButton))
+                    <div class="float-right">
+                        {{ $cardButton }}
                     </div>
-                    @if(isset($cardButton))
-                        <div class="col-4">
-                            {{ $cardButton }}
-                        </div>
-                    @endif
-                </div>
+                @endif
             </div>
         </div>
         <div class="card-body px-1 px-sm-4">
