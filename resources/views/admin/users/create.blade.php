3b036a41c9e81b65e3a262200647e78eb96e63a7
Gerson Altamirano <gersonaltamirano@gmail.com>
2022-01-31 23:47:09 -0600

    user view with datatables in HTML

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -2,34 +2,38 @@
     <x-slot name="header">
         Create User
     </x-slot>
-    <x-slot name="pageHeader">
-        Create User
-        <i class="fal fa-tachometer-slow"></i>
-    </x-slot>
-    <x-slot name="cardTitle">
-        <i class="fal fa-layer-plus"></i>
-        <span class="ml-3">
-            Insert data for new User
-        </span>
-    </x-slot>
-    @include('adminlte-templates::common.errors')
-    <div class="card">
-        {!! Form::open(['route' => 'users.store', 'files' => true]) !!}
-        <div class="card-body">
-            <div class="row">
-                @include('admin.users.fields')
+    <div class="card card-primary card-outline min-vh-100">
+        <div class="card-header">
+            <div class="card-title ">
+                <h4>
+                    <i class="fal fa-layer-plus"></i>
+                    <span class="ml-3">
+                        Insert data for new User
+                    </span>
+                </h4>
             </div>
         </div>
-        <div class="card-footer">
-            <button type="submit" class="btn btn-outline-primary float-right">
-                <span class="px-2">Save</span>
-                <i class="fal fa-save"></i>
-            </button>
-            <a href="{{ route('users.index') }}" class="btn btn-outline-danger float-right mr-2" >
-                <span class="px-2">Cancel</span>
-                <i class="fal fa-ban"></i>
-            </a>
+        <div class="card-body">
+            @include('adminlte-templates::common.errors')
+            <div class="card">
+                {!! Form::open(['route' => 'users.store', 'files' => true]) !!}
+                <div class="card-body">
+                    <div class="row">
+                        @include('admin.users.fields')
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-outline-primary float-right">
+                        <span class="px-2">Save</span>
+                        <i class="fal fa-save"></i>
+                    </button>
+                    <a href="{{ route('users.index') }}" class="btn btn-outline-danger float-right mr-2" >
+                        <span class="px-2">Cancel</span>
+                        <i class="fal fa-ban"></i>
+                    </a>
+                </div>
+                {!! Form::close() !!}
+            </div>
         </div>
-        {!! Form::close() !!}
     </div>
 </x-admin-layout>
