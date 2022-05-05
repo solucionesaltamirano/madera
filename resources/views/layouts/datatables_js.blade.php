bd0a76db17d0feb00c6ed26f149d93824cb1fdf6
Gerson Altamirano <gersonaltamirano@gmail.com>
2022-05-05 08:57:27 -0600

    fix diseño

--- resources/views/layouts/datatables_js.blade.php
+++ resources/views/layouts/datatables_js.blade.php
@@ -1,9 +1,5 @@
 {{-- dataTables --}}
-<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.bootstrap4.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.colVis.min.js"></script>
+@include('layouts.datatables_html_js')
 <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
 
 
